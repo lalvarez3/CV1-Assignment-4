5f7576b1d860784d40f5a1292f5c738a9898fe8c
Coen <coenvandermeijs@live.nl>
2021-10-08 14:19:06 +0200

    adjusted keypointpmatching

--- keypoint_matching.py
+++ keypoint_matching.py
@@ -14,19 +14,20 @@ def keypoint_matching(img1, img2):
     # The first 3 steps can be performed using David Lowe's SIFT
     # return keypoints
 
-    gray= cv2.cvtColor(img1,cv2.COLOR_BGR2GRAY)
+    gray1= cv2.cvtColor(img1,cv2.COLOR_BGR2GRAY)
     sift = cv2.SIFT_create()
     kp = sift.detect(gray,None)
     img1=cv2.drawKeypoints(gray,kp,img1)
 
-    gray= cv2.cvtColor(img2,cv2.COLOR_BGR2GRAY)
+    gray2= cv2.cvtColor(img2,cv2.COLOR_BGR2GRAY)
     sift = cv2.SIFT_create()
     kp, des = sift.detectAndCompute(gray,None)
     img2=cv2.drawKeypoints(gray,kp,img2)
-    print(des)
+    print(des.shape)
     # plt.imshow(img1)
     # plt.show()
-
+    matcher = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
+    matches = matcher.match(des1,des2)
     # plt.imshow(img2)
     # plt.show()
 
